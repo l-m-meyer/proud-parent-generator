578726a3a0b8df51e536bd75425492f715905243
Lisa Meyer <lisamermey@gmail.com>
2022-06-21 11:47:13 -0400

    Update tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders proud parent logo text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/proud parent/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoText = screen.getByText(/proud parent/i);
+  expect(logoText).toBeInTheDocument();
 });
