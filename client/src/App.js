df7a43aa43557dab4dd34410343f0188c6593b74
Lisa Meyer <lisamermey@gmail.com>
2022-06-21 13:15:40 -0400

    Import Quote and Button components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Quote from './components/Quote';
+import Button from './components/Button';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Proud Parent</h1>
+      <Quote />
+      <Button />
     </div>
   );
 }
