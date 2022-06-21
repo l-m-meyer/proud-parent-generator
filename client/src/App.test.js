27e10527347ab74a2420c5a0ebfeaac0c1ebbb69
Lisa Meyer <lisamermey@gmail.com>
2022-06-21 12:17:14 -0400

    Test passes, App renders without crashing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 afterEach(cleanup);
 
+test('renders App without crashing', () => {
+  render(<App />);
+});
+
 test('renders proud parent logo text', () => {
   render(<App />);
   const logoText = screen.getByText(/proud parent/i);
