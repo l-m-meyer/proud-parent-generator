27acd000469d6677e43e53be6bbde4b5f3a271e9
Lisa Meyer <lisamermey@gmail.com>
2022-06-22 15:33:27 -0400

    Import and use cors

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { ENVIRONMENT, PORT } = process.env;
+const cors = require(cors);
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -8,6 +9,7 @@ const app = express();
 
 // middleware
 app.use(morgan(ENVIRONMENT));
+app.use(cors());
 app.use(bodyParser.json());
 
 // routes import
