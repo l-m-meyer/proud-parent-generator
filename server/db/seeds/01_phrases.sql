1a93c4857da29f1fde3e7d325ebf33353335c539
Lisa Meyer <lisamermey@gmail.com>
2022-06-22 22:08:27 -0400

    Add seed phrases

--- server/db/seeds/01_phrases.sql
+++ server/db/seeds/01_phrases.sql
@@ -1,1 +1,4 @@
-INSERT INTO phrases (phrase) VALUES ("I'm proud of you!");
+INSERT INTO phrases (phrase) VALUES ("I am so proud of you!");
+INSERT INTO phrases (phrase) VALUES ("I know things are difficult right now, but I also know you've got what it takes to get through it.");
+INSERT INTO phrases (phrase) VALUES ("Sending good thoughts your way. I believe in you and don't doubt for a minute that you will kill it.");
+INSERT INTO phrases (phrase) VALUES ("What matters is that you're trying.");
