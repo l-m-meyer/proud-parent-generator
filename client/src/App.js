d03ce06d940adeba57df3b7cd7460bb33c57a0b0
Lisa Meyer <lisamermey@gmail.com>
2022-06-28 12:25:21 -0400

    Fetch phrases from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Quote from './components/Quote';
 import Button from './components/Button';
 
 
 function App() {
+  const [phrase, setPhrase] = useState();
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/phrases')
+      .then((res) => {
+        setPhrase(res.data.phrase);
+      })
+  }, [])
   return (
     <div className="App">
       <h1>Proud Parent</h1>
-      <Quote />
+      <Quote phrase={phrase} />
       <Button />
     </div>
   );
