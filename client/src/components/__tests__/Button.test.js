d038f3eef319a63a9e6944b19bc3162e53d63cbe
Lisa Meyer <lisamermey@gmail.com>
2022-06-30 12:38:02 -0400

    Tests pass

--- client/src/components/__tests__/Button.test.js
+++ client/src/components/__tests__/Button.test.js
@@ -7,3 +7,13 @@ afterEach(cleanup);
 test('Button renders without crashing', () => {
   render(<Button />);
 });
+
+test('Button renders its `children` prop as text', () => {
+  const { getByText } = render(<Button>Default</Button>);
+  expect(getByText('Default')).toBeInTheDocument();
+});
+
+test('Button renders a default button style', () => {
+  const { getByText } = render(<Button>Default</Button>);
+  expect(getByText('Default')).toHaveClass('btn');
+});
